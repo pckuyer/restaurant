9402bd505c3598b2275a6ec196817e3670d4429d
Paul Kuyer <paul.kuyer@tuta.io>
2022-12-03 12:17:49 +0000

    set up dev tool and wathching

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import printMe from './menu.js'
 
-alert("dag Paulie")
-
 function component() {
   const element = document.createElement('div');
 
   const btn = document.createElement('button');
 
-  element.innerHTML = "this is the innerhtml";
+  element.innerHTML = "Hello Everyone! This is the innerhtml";
 
   btn.innerHTML = 'click me and check the console';
   btn.onclick = printMe;
@@ -20,6 +18,3 @@ function component() {
 document.body.appendChild(component());
 
 
-const paul = document.createElement('p');
-paul.innerHTML = "tweede bericht";
-document.querySelector('#content').appendChild(paul); 
